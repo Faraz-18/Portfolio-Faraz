e578bf2899cc945e7dfeee2c50d5a00c47bb671f
Syed Faraz Ahmed <143342902+Faraz-18@users.noreply.github.com>
2025-09-15 01:35:54 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -159,7 +159,7 @@ As a GSSoC’25 contributor, I gained hands-on experience in open-source collabo
           <div className="relative">
 
             <img
-              src="\Public\PP.jpg"   
+              src="./Public/PP.jpg"   
               alt="Profile"
               className="rounded-2xl shadow-lg max-w-xs md:max-w-sm lg:max-w-md "
             />
